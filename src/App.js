219f23c63076b029f8b19b43e970250c3a3f0057
ng1n3 <code.with.muyiwa@gmail.com>
2024-02-14 12:00:45 +0100

    Add, increase done

--- src/App.js
+++ src/App.js
@@ -1,50 +1,112 @@
 import { useState } from "react";
 
-const initalTodos = [
-  { id: 1, item: "Sony Play Station 5", total: 1 },
-  { id: 2, item: "DualShock controller 5", total: 3 },
-  { id: 3, item: "Apple macbook Pro 2024", total: 1 },
-];
+// const initalTodos = [
+//   { id: 1, item: "Sony Play Station 5", total: 1 },
+//   { id: 2, item: "DualShock controller 5", total: 3 },
+//   { id: 3, item: "Apple macbook Pro 2024", total: 1 },
+// ];
 
 function App() {
   return <Todo />;
 }
 
 function Todo() {
+  const [name, setName] = useState("");
+  const [carts, setcarts] = useState([]);
 
+  function handleAddcart(cart) {
+    setcarts((prevcart) => [...prevcart, cart]);
+  }
+
+  const id = crypto.randomUUID();
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newCart = {
+      id,
+      item: name,
+      total: 1,
+      selected: false,
+    };
+
+    handleAddcart(newCart);
+    setName("");
+  }
+
+  function handleCross(id) {
+    setcarts((prevcart) =>
+      prevcart.map((cart) =>
+        cart.id === id ? { ...cart, selected: !cart.selected } : cart
+      )
+    );
+  }
 
   return (
-    <div>
-      <ul>
-        {initalTodos.map((todo) => (
-          <TodoItem todo={todo} key={todo.id} />
-        ))}
-      </ul>
-    </div>
+    <>
+    <h1> QUICK SHOPPING CART</h1>
+      <div className="Todo">
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add your item"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit">Add</button>
+        </form>
+        <ul>
+          {carts.map((cart) => (
+            <TodoItem
+              cart={cart}
+              key={cart.id}
+              onCross={() => handleCross(cart.id)}
+            />
+          ))}
+        </ul>
+      </div>
+      <Footer carts={carts} />
+    </>
   );
 }
 
-function TodoItem({ todo}) {
+function TodoItem({ cart, onCross }) {
   const [count, setCount] = useState(0);
 
-  function handleCountIncrement() {
-    setCount(() => count + 1)
+  function handleCountIncrement(e) {
+    e.stopPropagation()
+    setCount(() => count + 1);
   }
 
-  function handleCountDecrement() {
-    setCount(() => count - 1)
+  function handleCountDecrement(e) {
+    e.stopPropagation()
+    setCount(() => count - 1);
   }
-  console.log(todo);
+
   return (
     <>
-    <li>
-      <h3>{todo.item}</h3>
-      <button onClick={handleCountDecrement}>-</button>
-      <span>{todo.total + count}</span>
-      <button onClick={handleCountIncrement}>+</button>
-    </li>
-    <p>You haven't packed some items, you still have X items left</p>
+      <li
+        className={cart.selected ? "item selected" : "item"}
+        onClick={onCross}
+      >
+        <h3>{cart.item}</h3>
+        <div>
+          <button onClick={handleCountDecrement}>-</button>
+          <span> {cart.total + count} </span>
+          <button onClick={handleCountIncrement}>+</button>
+        </div>
+      </li>
     </>
+  );
+}
+
+function Footer({ carts }) {
+  const remainingItems = carts.filter((cart) => !cart.selected).length;
+  return (
+    <div className="footer">
+      <p>
+        You still have {remainingItems} items in your shopping list.
+      </p>
+      ;
+    </div>
   );
 }
 
